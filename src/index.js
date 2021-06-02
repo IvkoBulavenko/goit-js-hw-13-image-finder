e16b200069583251475b10f8e6cee30f1d0c8c65
Ivan Bulavenko <bulavenkoivan@gmail.com>
2021-06-02 11:59:20 +0300

    endhw

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import './js/apiService';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
+import 'basiclightbox/dist/basicLightbox.min.css';
